ec2eed34c1d93e32da0cd0e0abb1f5b2454738f1
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2017-06-30 09:23:47 +0200

    simple test case

--- client_test.go
+++ client_test.go
@@ -3,5 +3,11 @@ package main
 import "testing"
 
 func TestNewClient(t *testing.T) {
-
+	url := "http://testhappymeter.cloudapp.net/api/storehappydocument"
+	comment := "Happymeter device, ip: " + getMyIP()
+	tags := "testing"
+	_, err := NewClient(url, tags, comment)
+	if err != nil {
+		panic(err)
+	}
 }
